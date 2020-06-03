2d10b0334c66fe9c084215d0cbbf477625e4121f
pbryzek <bryzek@gmail.com>
2020-06-02 22:07:13 -0700

    13.4.3

--- mapping-lines/static/js/logic.js
+++ mapping-lines/static/js/logic.js
@@ -1,26 +1,39 @@
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // We create the tile layer that will be the background of our map.
+
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  accessToken: API_KEY
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
 });
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// Get data from cities.js
-let cityData = cities;
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [30.1975, -97.6664],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
+];
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function (city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    "radius": city.population/100000,
-    "lineWeight": 4,
-    color: "orange"
+let layout = {
+  color: "transparent",
+  weight: 3,
+  lineCap: 'square',
+  fillOpacity: 1,
 
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+};
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, layout).addTo(map);
+
+let layout2 = {
+  color: "blue",
+  weight: 3,
+  dashArray: '5, 10',
+  lineCap: 'square'
+};
+L.polyline(line, layout2).addTo(map);
