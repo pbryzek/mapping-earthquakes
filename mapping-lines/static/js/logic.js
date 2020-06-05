47890b722865e482243cc3460be4a7dade79654d
pbryzek <bryzek@gmail.com>
2020-06-04 22:54:42 -0700

    add in changes

--- mapping-lines/static/js/logic.js
+++ mapping-lines/static/js/logic.js
@@ -44,7 +44,7 @@ let geojsonFeature = {
 //let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   accessToken: API_KEY
@@ -65,7 +65,7 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center: [30, 30],
+	center: [44.0, -80.0],
 	zoom: 2,
 	layers: [streets]
 });
@@ -80,6 +80,22 @@ streets.addTo(map);
 let airportData = "https://raw.githubusercontent.com/pbryzek/mapping-earthquakes/master/majorAirports.json";
 
 // Grabbing our GeoJSON data.
+d3.json(airportData).then(function (data) {
+  console.log(data);
+
+  // Creating a GeoJSON layer with the retrieved data.
+  /*
+  L.geoJson(data, {
+    onEachFeature: function (feature, layer) {
+      let popupH1 = "<h2>" + "Airport code: " + feature.properties.faa + "</h2>";
+      let popupH2 = "<h2>" + "Airport name: " + feature.properties.name + "</h2>";
+      let popupStr = popupH1 + "<hr>" + popupH2
+      layer.bindPopup(popupStr);
+    }
+  }).addTo(map);
+});
+*/
+
 d3.json(airportData).then(function (data) {
   console.log(data);
 
